ce45e5538677b65c42c1cd9d1df76ebd608e71e1
Jianqi Pan <jannchie@gmail.com>
2024-04-23 21:24:10 +0900

    :sparkles: feat(pwa): add more icons

--- vite.config.ts
+++ vite.config.ts
@@ -30,15 +30,31 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
       manifest: {
-        name: 'Gemini Style Chat',
-        short_name: 'Gemini Chat',
+        name: 'Chat Gemini UI',
+        short_name: 'Chat G',
         description: 'A chat application built with Gemini UI Style',
         theme_color: '#131314',
         icons: [
           {
-            src: 'favicon.png',
-            sizes: '256x256',
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
             type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
           },
         ],
       },
